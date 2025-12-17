fec61307cb328fa59462562992924126f9acff72
vladimir858 <standardwebsites@gmail.com>
2025-12-17 07:41:03 +0300

    feat JS

---
+++ src/script.js
@@ -0,0 +1,24 @@
+class Todo{
+selectors = {
+    root: "[data-js-todo]",
+    newTaskForm: '[data-js-todo-new-task-form]',
+    newTaskInput: '[data-js-todo-new-task-input]',
+    searchTaskForm: '[data-js-todo-search-task-form]',
+    searchTaskInput: '[data-js-todo-search-task-input]',
+    totalTasks: '[data-js-todo-total-tasks]',
+    deleteAllButton: '[data-js-todo-delete-all-button]',
+    list: '[data-js-todo-list]',
+    item: '[data-js-todo-item]',
+    itemCheckbox: '[data-js-todo-item-checkbox]',
+    itemLabel: '[data-js-todo-item-label]',
+    itemDeleteButton: '[data-js-todo-item-delete-button]',
+    emptyMessage: '[data-js-todo-empty-message]',
+};
+ stateClasses = {
+    isVisible: 'is-visible',
+    isDisappearing: 'is-disappering',
+ }
+  localStorageKey = 'todo-items'
+} 
+
+new Todo()
